69f7eac4d79e0e4bd94e22b3970a7981947d7dfb
levi <levinkipyego@gmail.com>
2024-07-20 15:33:09 +0300

    second commit

--- src/components/assets/all_products.js
+++ src/components/assets/all_products.js
@@ -1,4 +1,4 @@
-import img_1 from"./image_1"
+import img_1 from"./image_1.png"
 
 let all_products  = [
     {
@@ -30,6 +30,7 @@ let all_products  = [
 
     {
         id: 1,
+        name: "women necklace",
         category: "women",
         image: img_1,
         new_price: 50.0,
@@ -38,9 +39,10 @@ let all_products  = [
 
     {
         id: 1,
+        name: "women necklace",
         category: "women",
         image: img_1,
         new_price: 50.0,
         old_price: 80.50,
     },
-]
+];
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,28 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import './Navbar.css'
+
+import cart_logo from '../assets/cart_2.png'
 
 const Navbar = () => {
   return (
-    <div className='navbar'>Navbar</div>
+    <div className='navbar'>
+      <div className="nav-logo">
+        <img src="" alt="" />
+        <p>SHOPPER</p>
+        </div>
+
+        <ul className='nav-menu'>
+          <li>Shop</li>
+          <li>Men</li>
+          <li>Women</li>
+          <li>Kids</li>
+        </ul>
+        <div className="nav-login-cart">
+          <button>Login</button>
+          <img src={cart_logo} alt="" />
+        </div>
+    </div>
   )
 }
 
