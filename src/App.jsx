74e129202aaa7cdbb9263242c3119993a470d76d
levi <levinkipyego@gmail.com>
2024-07-20 17:59:52 +0300

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 /* eslint-disable no-unused-vars */
 import { useState } from 'react'
-
+import { BrowserRouter, Routes, Route } from  'react-router-dom'
 import './App.css'
 import Navbar from './components/navbar/Navbar'
 import Footer from './components/footer/Footer'
+import Shop from './pages/Shop'
+import ShopCategory from './pages/ShopCategory'
 
 
 function App() {
@@ -11,7 +13,19 @@ function App() {
 
   return (
     <div>
-    <Navbar/>
+    
+    <BrowserRouter>
+      <Navbar/>
+      
+      <Routes>
+          <Route path="/"  element = {<Shop/>}></Route>
+          <Route path="/Men"  element = {<ShopCategory category = "Men"/>}></Route>
+          <Route path="/"  element = {<ShopCategory category = "Women"/>}></Route>
+          <Route path="/"  element = {<ShopCategory category = "Kids"/>}></Route>
+          <Route path="/"  element = {<Shop/>}></Route>
+          <Route path="/"  element = {<Shop/>}></Route>
+      </Routes>
+    </BrowserRouter>
     <Footer/>
     </div>
   )
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,14 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React , { useState } from 'react'
+
 import './Navbar.css'
 
-import cart_logo from '../assets/cart_2.png'
+import cart_logo from '../assets/cart_1.png'
 
 const Navbar = () => {
+
+const [menu, setMenu] = useState("shop")
+
   return (
     <div className='navbar'>
       <div className="nav-logo">
@@ -13,14 +17,15 @@ const Navbar = () => {
         </div>
 
         <ul className='nav-menu'>
-          <li>Shop</li>
-          <li>Men</li>
-          <li>Women</li>
-          <li>Kids</li>
+          <li onClick={()=>{setMenu("shop")}}>Shop {menu === "shop" ? <hr/> :<></>}</li>
+          <li onClick={()=>{setMenu("Men")}}>Men {menu === "Men" ? <hr/>:<></>}</li>
+          <li onClick={()=>{setMenu("Women")}}>Women {menu === "Women" ? <hr/>:<></>}</li>
+          <li onClick={()=>{setMenu("Kids")}}>Kids {menu === "Kids" ? <hr/>:<></>}</li>
         </ul>
         <div className="nav-login-cart">
           <button>Login</button>
           <img src={cart_logo} alt="" />
+          <div className="nav-cat-count">0</div>
         </div>
     </div>
   )
