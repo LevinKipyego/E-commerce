2c051c917011d3abc244d1829dbd5d961bbd7242
levi <levinkipyego@gmail.com>
2024-07-21 15:13:16 +0300

    hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,25 +1,30 @@
 import React from 'react'
 import './Hero.css'
 
+import hero_image from '../assets/hero/900.jpg'
+
 const Hero = () => {
   return (
     <div className='hero'>
         <div className="hero-left">
-            <h2>NEW AND CLASSY ONES ONLY</h2>
-            <div>
+            <h4>NEW AND CLASSY ONES ONLY</h4>
+            <div className='sec2'>
                 <div className="hand-hand-icon">
                     <p>new</p>
-                    <img src="" alt="" />
+                    
                 </div>
                 <p>collection</p>
                 <p>for everyone</p>
+                <p>Keep checking it  out</p>
             </div>
+            
             <div className="hero-latest-btn">
                 <div>Latest Collection</div>
+                <img src="" alt="" />
             </div>
         </div>
         <div className="hero-right">
-            <img src="" alt="" />
+            <img src= { hero_image } alt="" />
         </div>
     </div>
   )
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,6 +4,7 @@ import React , { useState } from 'react'
 
 import './Navbar.css'
 
+import shopper_logo from '../assets/navbar/shopper2.png'
 import cart_logo from '../assets/cart_1.png'
 import { Link } from 'react-router-dom'
 
@@ -14,7 +15,7 @@ const [menu, setMenu] = useState("shop")
   return (
     <div className='navbar'>
       <div className="nav-logo">
-        <img src="" alt="" />
+        <img className='shopper-logo' src= { shopper_logo } alt="" />
         <p>SHOPPER</p>
         </div>
 
@@ -26,7 +27,7 @@ const [menu, setMenu] = useState("shop")
         </ul>
         <div className="nav-login-cart">
           <button><Link Link style={{textDecoration:'none'}} to='/login'>Login</Link></button>
-          <Link Link style={{textDecoration:'none'}} to='/cart'><img src={cart_logo} alt="" /></Link>
+          <Link Link style={{textDecoration:'none'}} to='/cart'><img  className='cart-logo' src={cart_logo} alt="" /></Link>
           <div className="nav-cat-count">0</div>
         </div>
     </div>
