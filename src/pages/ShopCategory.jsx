12832fe054a4bc9f1ec8de8923d835a8935e219e
levi <levinkipyego@gmail.com>
2024-07-24 13:47:02 +0300

    shop category

--- src/pages/ShopCategory.jsx
+++ src/pages/ShopCategory.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import './ShopCategory.css'
+import women_banner from '../components/assets/banners/Artboard 1.png'
 
 const ShopCategory = () => {
   return (
-    <div>ShopCategory</div>
+    <div className = 'ShopCategory'>
+      <img src =  { women_banner } alt="" />
+    </div>
   )
 }
 
